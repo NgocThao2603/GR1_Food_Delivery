9cfb2a2bb4eda744ca887377a01f2500d6ae65af
NgocThao26 <ngocthao22062003@gmail.com>
2024-07-13 17:40:37 +0700

    update admin function

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,26 +1,28 @@
-import React from 'react';
+import React from "react";
 // Ensure the relative paths to Navbar and Sidebar components are correct
-import Navbar from './components/Navbar/Navbar'; // Check this path
-import Sidebar from './components/Sidebar/Sidebar'; // Check this path
-import { Routes, Route } from 'react-router-dom';
-import Add from './pages/Add/Add';
-import Orders from './pages/Orders/Orders';
-import List from './pages/List/List';
-import { ToastContainer} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import Navbar from "./components/Navbar/Navbar"; // Check this path
+import Sidebar from "./components/Sidebar/Sidebar"; // Check this path
+import { Routes, Route } from "react-router-dom";
+import Add from "./pages/Add/Add";
+import Orders from "./pages/Orders/Orders";
+import List from "./pages/List/List";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const App = () => {
+  const url = "http://localhost:4000";
   return (
     <div>
       <ToastContainer />
-      <Navbar/>
-     <hr/>
-      <div className="app-content"> {/* Ensure styles for app-content are correctly applied */}
+      <Navbar />
+      <hr />
+      <div className="app-content">
+        {" "}
+        {/* Ensure styles for app-content are correctly applied */}
         <Sidebar /> {/* Ensure Sidebar component is correctly exported */}
         <Routes>
-          <Route path="/add" element={<Add/>} />
-          <Route path="/list" element={<List/>} />
-          <Route path="/orders" element={<Orders/>} />
-
+          <Route path="/add" element={<Add url={url} />} />
+          <Route path="/list" element={<List url={url} />} />
+          <Route path="/orders" element={<Orders url={url} />} />
         </Routes>
       </div>
     </div>
--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -1,33 +1,45 @@
-import React from 'react';
-import './Sidebar.css';
-import { assets } from '../../assets/assets';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import "./Sidebar.css";
+import { assets } from "../../assets/assets";
+import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <div>
-      <div className='sidebar'>
+      <div className="sidebar">
         <div className="sidebar-options">
-          <NavLink to='/add' className="sidebar-option">
-            <img src={assets.add_icon} alt="" style={{ width: '30px', height: 'auto' }} />
+          <NavLink to="/add" className="sidebar-option">
+            <img
+              src={assets.add_icon}
+              alt=""
+              style={{ width: "30px", height: "auto" }}
+            />
             <p>Add Items</p>
           </NavLink>
         </div>
         <div className="sidebar-options">
-          <NavLink to='/list' className="sidebar-option">
-            <img src={assets.order_icon} alt="" style={{ width: '30px', height: 'auto' }} />
+          <NavLink to="/list" className="sidebar-option">
+            <img
+              src={assets.list_icon}
+              alt=""
+              style={{ width: "30px", height: "auto" }}
+            />
             <p>List Items</p>
           </NavLink>
         </div>
         <div className="sidebar-options">
-          <NavLink to='/orders' className="sidebar-option">
-            <img src={assets.order_icon} alt="" style={{ width: '30px', height: 'auto' }} />
+          <NavLink to="/orders" className="sidebar-option">
+            <img
+              src={assets.order_icon}
+              alt=""
+              style={{ width: "40px", height: "auto" }}
+            />
             <p>Orders</p>
           </NavLink>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Sidebar;
